2a7944b8296ffd16525e08daae0908897f75a084
zhiliang hou <bughou@gmail.com>
2015-12-02 13:31:20 +0800

    save

--- jquery.tip.js
+++ jquery.tip.js
@@ -4,30 +4,36 @@
  * @param {String|DOM|jQueryObject} content the content for html method
  * @param {Object} options tip options
  * {
- *   timeout: 5, //seconds
  *   place:   'right', 'top', 'left', 'bottom',
  *            'right top',    'right bottom',
  *            'top left',     'top right',
  *            'left top',     'left bottom',
  *            'bottom left',  'bottom right'
+ *   time:     5, //seconds,
+ *   show_on: 'mouseenter',
+ *   hide_on: { tip: 'mouseout', target: 'change' }
  * }
  */
 
 (function ($) {
-  $.fn.tip = function (content, options) {
+  $.fn.tip = tip;
+
+  function tip (content, options) {
+    options = $.extend({}, tip.defaults, options);
     var $target = this.eq(0);
     var $tip = get_or_create_tip($target, content);
-    if (content) {
-      $tip.html('<i class="jq-tip-arrow"></i>');
-      $tip.append(content);
-      if (options && options.place) {
-        set_pos($tip, $target, $.trim(options.place));
-      } else {
-        set_pos($tip, $target, '');
-      }
-    }
+    if (!content) return $tip;
+
+    $tip.html('<i class="jq-tip-arrow"></i>');
+    $tip.append(content);
+    set_pos($tip, $target, options.place);
+    set_show($tip, $target, options.show_on);
+    set_hide($tip, $target, options.hide_on);
+    set_timer($tip, options.time);
     return $tip;
   };
+
+  tip.defaults = { hide_on: { target: 'change' } };
 
   var $container;
 
@@ -43,18 +49,19 @@
     return $tip;
   }
 
-  var places = [
-    'top', 'right', 'bottom', 'left',
-    'top left',     'top right',
-    'right top',    'right bottom',
-    'bottom left',  'bottom right',
-    'left top',     'left bottom'
-  ];
+  // [
+  //   'top', 'right', 'bottom', 'left',
+  //   'top left',     'top right',
+  //   'right top',    'right bottom',
+  //   'bottom left',  'bottom right',
+  //   'left top',     'left bottom'
+  // ];
 
   function set_pos($tip, $target, place) {
     $tip.removeClass('left right top bottom');
+    place = place ? $.trim(place).split(/\s+/) : '';
+
     var offset = $target.offset();
-    var place = place.split(/\s+/);
     switch(place[0]) {
       case 'right':
         pos_right(offset, $tip, $target, place[1]);
@@ -68,7 +75,7 @@
       default:
         pos_top(offset, $tip, $target, place[1]);
     }
-    $tip.offset(offset).show();
+    $tip.offset(offset);
   }
 
   function try_pos() {
@@ -129,5 +136,33 @@
       default:
         offset.left += ($target.outerWidth() - $tip.outerWidth()) / 2;
     }
+  }
+
+  function set_show($tip, $target, event) {
+    if (!event) {
+      $tip.show(); return;
+    }
+    $target.on(event, function() {
+      $tip.show();
+    });
+  }
+
+  function set_hide($tip, $target, event) {
+    if (event.tip) $tip.on(event.tip, function () {
+      $tip.hide();
+    });
+    if (event.target) $target.on(event.target, function () {
+      $tip.hide();
+    });
+  }
+
+  function set_timer($tip, time) {
+    if (!time) return;
+    var timer = $tip.data('timer');
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(function() {
+      $tip.hide();
+    }, time * 1000);
+    $tip.data('timer', timer);
   }
 })(jQuery);
